49d1ecace3b731ea7ee6b78cdad5a10868f3ae06
huang-kai <kyne.huang@aliyun.com>
2024-02-18 18:23:10 +0800

    change name

--- src/mini_mec_six_arm_moveit_config/launch/static_virtual_joint_tfs.launch.py
+++ src/mini_mec_six_arm_moveit_config/launch/static_virtual_joint_tfs.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_static_virtual_joint_tfs_laun
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("mini_mec_six_arm", package_name="mini_mec_six_arm_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("mini_mec_six_arm", package_name="mini_mec_six_arm_moveit_config").to_moveit_configs()
     return generate_static_virtual_joint_tfs_launch(moveit_config)
--- src/mini_mec_six_arm_moveit_config/launch/warehouse_db.launch.py
+++ src/mini_mec_six_arm_moveit_config/launch/warehouse_db.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_warehouse_db_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("mini_mec_six_arm", package_name="mini_mec_six_arm_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("mini_mec_six_arm", package_name="mini_mec_six_arm_moveit_config").to_moveit_configs()
     return generate_warehouse_db_launch(moveit_config)
